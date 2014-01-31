23ea9931d6bbd2e86f87d3e7adb59784f9006275
Peter Kneale <PeterKneale@gmail.com>
2014-01-31 22:30:43 +1100

    Using HTML Helpers can cut down on repetitive code

---
+++ source/SocialGoal.Web.Core/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using SocialGoal.Web.Core.Helpers;
+
+namespace SocialGoal.Web.Core.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static UserHtmlHelper User(this HtmlHelper html)
+        {
+            return new UserHtmlHelper(html, new UrlHelper(html.ViewContext.RequestContext));
+        }
+    }
+}
---
+++ source/SocialGoal.Web.Core/Helpers/BaseHtmlHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace SocialGoal.Web.Core.Helpers
+{
+    public class BaseHtmlHelper
+    {
+        private readonly HtmlHelper _html;
+        private readonly UrlHelper _url;
+
+        public BaseHtmlHelper(HtmlHelper html, UrlHelper url)
+        {
+            _html = html;
+            _url = url;
+        }
+        protected HtmlHelper Html { get { return _html; } }
+        protected UrlHelper Url { get { return _url; } }
+    }
+}
