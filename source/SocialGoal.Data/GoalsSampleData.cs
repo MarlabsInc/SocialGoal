9c25a650f0d2214b313703e53febb448c17da858
adarsh-vs <vs_adarsh@yahoo.com>
2014-02-20 19:42:04 +0530

    Removed seed data for admin user

--- source/SocialGoal.Data/GoalsSampleData.cs
+++ source/SocialGoal.Data/GoalsSampleData.cs
@@ -12,37 +12,6 @@ namespace SocialGoal.Data
     {
         protected override void Seed(SocialGoalEntities context)
         {
-           
-            var UserManager = new UserManager<ApplicationUser>(new
-
-                                                UserStore<ApplicationUser>(context));
-
-            var RoleManager = new RoleManager<IdentityRole>(new
-                                       RoleStore<IdentityRole>(context));
-
-            string name = "Admin";
-            string password = "123456";
-
-
-            //Create Role Admin if it does not exist
-            if (!RoleManager.RoleExists(name))
-            {
-                var roleresult = RoleManager.Create(new IdentityRole(name));
-            }
-
-            //Create User=Admin with password=123456
-            var user = new ApplicationUser();
-            user.UserName = name;
-            var adminresult = UserManager.Create(user, password);
-
-            //Add User Admin to Role Admin
-            if (adminresult.Succeeded)
-            {
-                var result = UserManager.AddToRole(user.Id, name);
-            }
-            //base.Seed(context);
-            context.Commit();
-
             new List<Metric>
             {
                 new Metric { Type ="%"},
